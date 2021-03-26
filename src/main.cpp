d558225d8c71165e2b954cb8c21512efbd197883
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2021-03-26 11:03:24 -0300

    Add base full verification
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "../inc/global.hpp"
@@ -19,9 +20,59 @@ void scan_clausulas() {
   }
 }
 
+void para_verdadeiro(int var){
+  variaveis[to_index(var)].valor = true;
+  variaveis[to_index(var * -1)].valor = false;
+}
+
+void verifica_clausula(){
+  bool resultado_final = true;
+  for(int i = 0; i < numero_clausulas; i++){
+    int tamanho = clausulas[i].variaveis.size();
+    bool resultado_clausula = false;
+    for(int j = 0; j < tamanho; j++){
+      resultado_clausula = resultado_clausula | clausulas[i].variaveis[j]->valor;
+      if(resultado_clausula)break;
+    }
+    if(resultado_clausula){
+      clausulas[i].valor = resultado_clausula;
+    }
+    resultado_final = resultado_final & resultado_clausula;
+    if(!resultado_final)break;
+  }
+
+  cout << resultado_final << endl;
+
+}
+
+void comando_full(){
+  for(int i = 0; i < numero_variaveis; i++){
+    int var;
+    cin >> var;
+    para_verdadeiro(var);
+  }
+  verifica_clausula();
+}
+
+void comando_flip(){
+
+}
+
 int main() {
   int numero_clausulas, numero_variaveis;
   cin >> numero_variaveis >> numero_clausulas;
 
   scan_clausulas();
+
+  string comando;
+
+  while(cin >> comando){
+    if(comando.compare("full") == 0){
+      comando_full();
+    }
+    else if(comando.compare("flip") == 0){
+      comando_flip();
+    }
+  }
+
 }
